40d2747ee51e8495abb06884116ab2366d777841
dalay <dalay@drupalka.ru>
2019-02-25 20:46:53 +0300

    last changes

--- js/main.js
+++ js/main.js
@@ -55,31 +55,11 @@ jQuery(document).ready(function($) {
     dots:false,
 		autoplay:false  
 	});  
-/*
-	dotcount = 1;
-	
-	jQuery('.owl-dot').each(function() {
-		jQuery( this ).addClass( 'dotnumber' + dotcount);
-		jQuery( this ).attr('data-info', dotcount);
-		dotcount=dotcount+1;
-	});
-	
-	slidecount = 1;
-	
-	jQuery('.owl-item').not('.cloned').each(function() {
-		jQuery( this ).addClass( 'slidenumber' + slidecount);
-		slidecount=slidecount+1;
-	});
-	
-	jQuery('.owl-dot').each(function() {	
-		grab = jQuery(this).data('info');		
-		slidegrab = jQuery('.slidenumber'+ grab +' img').attr('src');
-		jQuery(this).css("background-image", "url("+slidegrab+")");  	
-	});
-	
-	amount = $('.owl-dot').length;
-	gotowidth = 100/amount;			
-	jQuery('.owl-dot').css("height", gotowidth+"%");
-  */
+  
+  $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+  $(this)
+    .addClass('active').siblings().removeClass('active')
+    .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+  });
   
 });
--- templates/node/node--product.tpl.php
+++ templates/node/node--product.tpl.php
@@ -51,7 +51,7 @@ if ($discount_percent) {
 
 <div id="node-<?php print $node->nid; ?>" class="row <?php print $classes; ?>"<?php print $attributes; ?>>
   <div class="columns">
-    <div class="column col-6 col-xs-12">
+    <div class="column col-6 col-sm-12">
       <!-- Project Slider -->
       <?php if ((isset($content["field_discount"][0]) and $content["field_discount"][0]["#markup"] != "0%") or $extra_10) : ?>
         <span class="onsale"><?php print '-' . $discount_percent . '%';?></span>
@@ -67,8 +67,51 @@ if ($discount_percent) {
     <?php print render($content['field_main_description']); ?>
     
     </div>
-    <div class="column col-6 col-xs-12">
-    
+    <div class="column col-6 col-sm-12">
+      <div class="tabs stock-tabs">
+        <ul class="tabs__caption">
+          <li class="active"><?php print t('Specifications'); ?></li>
+          <li><?php print t('Delivery'); ?></li>
+          <li><?php print t('Payment'); ?></li>
+          <li><?php print t('Warranty'); ?></li>
+        </ul>
+        <div class="tabs__content product-specifications active">
+          <div class="table-responsive">
+            <?php
+              unset($content['field_main_description'], $content['field_rating'], $content['field_tags'], $content['field_catalog'], $content['field_antiprice'], $content['field_sku_autocomplete'], $content['field_votes']);
+              $rows = array();
+              foreach($content as $key => $field){
+                if(strpos($key, 'field') !== FALSE && !empty($field)){
+                  $content[$key]['#label_display'] = 'hidden';
+                  $values = array();
+                  foreach(element_children($content[$key]) as $i) {
+                    $values[] = render($content[$key][$i]);
+                  }
+                  $rows[] = array($content[$key]['#title'], implode('<br/>', $values));
+                }
+              }
+              $weight = render($content['weight']);
+              if($weight) {
+                $rows[] = array(t('Weight'), $weight);
+              }
+              $dimensions = render($content['dimensions']);
+              if($dimensions) {
+                $rows[] = array(t('Dimensions'), $dimensions);
+              }
+              print theme('table', array('rows' => $rows, 'attributes' => array('class' => array('table table-striped'))));
+            ?>
+          </div> 
+        </div>
+        <div class="tabs__content product-delivery">
+            Содержимое 2 блока
+        </div>
+        <div class="tabs__content product-payment">
+            Содержимое 3 блока
+        </div>
+        <div class="tabs__content product-warranty">
+            Содержимое 4 блока
+        </div>        
+      </div>
     </div>
   </div>
   
