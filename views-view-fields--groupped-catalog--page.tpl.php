68a64e75f062f08e98aea8b7b425613742f3a789
dalay <dalay@drupalka.ru>
2019-03-15 17:51:22 +0300

    last changes

---
+++ views-view-fields--groupped-catalog--page.tpl.php
@@ -0,0 +1,4 @@
+<a class="model-variant" href="<?php print url('model/' . $fields['field_main_sku']->content . '/' . $fields['nid']->content); ?>">
+  <?php print $fields['uc_product_image']->content; ?>
+  <span class="stock-level"><?php print $stock_total; ?></span>
+</a>
